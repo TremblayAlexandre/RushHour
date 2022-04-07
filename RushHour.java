8c211ef04073b91984697549c5ea5fd60b6187af
Wertubald <101939619+Wertubald@users.noreply.github.com>
2022-04-06 20:54:56 -0400

    Update RushHour.java

--- RushHour.java
+++ RushHour.java
@@ -1,17 +1,47 @@
+import javax.swing.*;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class RushHour {
-    static String path = "C:\\Users\\tremb\\Desktop\\BOB";
-    static String fileIn = "Prog2.txt";
-    static String fileOut = "Prog3.txt";
-    static java.io.File fileI = new File(path+fileIn);
-    static java.io.File fileO = new File(path+fileOut);
+    static String path = "C:\\Users\\tremb\\Desktop\\Users";
+
+
     public static void main(String[] args) {
-        try {
-            UserCreator.createFiles("Josh","Bob", "Xavier", 12 ,13);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+        Boolean bool =false;
+        String username="";
+        System.out.println("Wanna create an account? Yes:1 No:2");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if(num==1){
+            System.out.println("Enter a username: ");
+            username = sc.next();
+            System.out.println("Enter the password:");
+            String password = sc.next();
+            try {
+                UserCreator.createFiles(username, password);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
         }
+        else if(num==2){
+            System.out.println("Enter a username: ");
+            username = sc.next();
+            System.out.println("Enter the password:");
+            String password = sc.next();
+            try {
+                bool = Login.login(username,password);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        else{
+            System.out.println("Bro??!??!");
+        }
+        if(bool){
+
+        }
+
+
     }
 }
