b7036436b4c8e35ed59c868e8d8aad6abc7bad2b
Wertubald <101939619+Wertubald@users.noreply.github.com>
2022-04-06 20:54:32 -0400

    Update UserCreator.java

--- UserCreator.java
+++ UserCreator.java
@@ -3,31 +3,33 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
 public class UserCreator {
-    static String path = "\\C:\\Users\\tremb\\Desktop\\";
+    static String path = "\\C:\\Users\\tremb\\Desktop\\Users\\";
 
     public static String getPath() {
         return path;
     }
 
-    public static void createFiles(String folderName, String username, String password, int points, int levels) throws FileNotFoundException {
-            //path += folderName;
-            File f1 = new File(path+folderName);
-            boolean bool = f1.mkdir();
-            path += folderName+"\\";
-          File usernameFile = new File(path+"username.txt");
-          fileWrite(usernameFile, username);
-          File passwordFile = new File(path+"password.txt");
-          fileWrite(passwordFile, password);
-          File pointsFile = new File(path+"username.txt");
-          fileWrite(pointsFile, points);
-          File levelFile = new File(path+"password.txt");
-          fileWrite(levelFile, levels);
+    public static void createFiles(String username, String password) throws FileNotFoundException {
+        //path += folderName;
+        File f1 = new File(path + username);
+        boolean bool = f1.mkdir();
+        path += username + "\\";
+        File usernameFile = new File(path + "username.txt");
+        fileWrite(usernameFile, username);
+        File passwordFile = new File(path + "password.txt");
+        fileWrite(passwordFile, password);
+        File pointsFile = new File(path + "pointsFile.txt");
+        fileWrite(pointsFile, 0);
+        File levelFile = new File(path + "levelFile.txt");
+        fileWrite(levelFile, 1);
     }
+
     public static void fileWrite(File file, String string) throws FileNotFoundException {
         PrintWriter st = new PrintWriter(file);
         st.print(string);
         st.close();
     }
+
     public static void fileWrite(File file, int num) throws FileNotFoundException {
         PrintWriter st = new PrintWriter(file);
         st.print(num);
